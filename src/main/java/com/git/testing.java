4380422c396f77a30ce1bf57208559e03090a289
Ajay <ajay.thirumalesh@gmail.com>
2024-08-31 12:41:21 +0530

    Upadate V.1.0

---
+++ src/main/java/com/git/testing.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class testing {
+
+	public static void main(String[] args) {
+		
+		int i =5;
+		String s ="Honeydewz";
+		
+		while(i<=6) {
+			System.out.println(s);
+			break;
+		}
+		
+	}
+
+}
